75c3099fd7f2dd800c74fa8582bb16f7d9d398a5
Vsevolod <Vsewolod.610@gmail.com>
2022-04-16 02:59:27 +0300

    improve estimate.py

--- python/estimate.py
+++ python/estimate.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 """"
 set of T0_i temperature --> Estimate T0
-
-require:    ../Result/Result.txt
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import emcee
+from multiprocessing import Pool
 from chainconsumer import ChainConsumer
+
 
 
 # data
 res = []
-with open('../Result/Result.txt') as f:
+with open('./Result/Result.txt') as f:
     for k, line in enumerate(f, start=1):
         if 'None' in line.split():
             continue
         res.append(line.split())
     res = np.array(res, dtype=float)
-n = res[:, 0]
-y = res[:, 1]
-yerr = res[:, [2, 3]]
+x = res[:, 0]
+y = res[:, 2]
+yerr = res[:, [3, 4]]
+
 
 #____________________________________mcmc_________________________________
 
@@ -35,28 +36,32 @@ def log_prior_gauss(v, mean, sigma_p, sigma_m):
     return - 0.5 * (v - mean) ** 2 / sigma ** 2
 
 
-def log_probability(v, y, yerr, prior=lambda v: 0):
-    if not np.isfinite(prior(v)):
+def log_probability(params, x, y, yerr, prior=lambda v: 0):
+    if not np.isfinite(prior(params)):
         return -np.inf
-    sigma2 = np.array([(yerr[i, 1] if v > yi else yerr[i, 0]) for i, yi in enumerate(y)]) ** 2
-    s2 = np.array([(yerr[i, 0] + yerr[i, 1]) / 2 for i, yi in enumerate(y)]) ** 2
-    return - 0.5 * np.sum([(yi - v) ** 2 / sigma2[i] + 2 * np.pi * s2[i]for i, yi in enumerate(y)]) + prior(v)
+    m = model(*params, x)
+    sigma2 =  np.array([(yerr[i, 1] if m[i] > yi else yerr[i, 0]) for i, yi in enumerate(y)]) ** 2
+    return - 0.5 * np.sum([(yi - m[i]) ** 2 / sigma2[i] for i, yi in enumerate(y)]) + prior(params)
+
+
+def model(T0, z):
+    return T0 + 0 * z
 
 # setting of mcmc
 nwalkers = 200
 nsteps = 500
-amputete = 100 # int(0.5 * nsteps)
+amputete = 100
 ndim = 1
 names_of_params = [r'T0']
 pos = [2.72] +  0.5 * np.random.randn(nwalkers, ndim)
-prior = lambda v: log_prior_box(v, 0, 20)
-log_prob = lambda v: log_probability(v, y, yerr, prior)
+
 # mcmc mechanism
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob)
-sampler.run_mcmc(pos, nsteps, progress=True)
-flat_samples = sampler.chain[:, amputete : , :].reshape((-1, ndim))
+with Pool() as pool:
+    sampler = emcee.EnsembleSampler( nwalkers, ndim, log_probability, args=(x, y, yerr), pool=pool)
+    sampler.run_mcmc(pos, nsteps, progress=True)
+flat_sample = sampler.chain[:, amputete : , :].reshape((-1, ndim))
 c = ChainConsumer()
-c.add_chain(flat_samples, parameters=names_of_params)
+c.add_chain(flat_sample, parameters=names_of_params)
 summary = c.analysis.get_summary(parameters=names_of_params)[names_of_params[0]]
 print(summary)
 
@@ -65,6 +70,7 @@ print(summary)
 # chainconsum pic
 fig = c.plotter.plot(display=False, legend=False, figsize=(6, 6))
 fig.savefig('T_0 dist.pdf', bbox_inches='tight')
+
 # MCchain pic
 fig, ax = plt.subplots(figsize=(10, 7), sharex=True)
 samples = sampler.get_chain()
@@ -72,20 +78,15 @@ ax.plot(samples[:, :, 0], "k", alpha=0.3)
 ax.set_xlabel(r'steps', fontsize=12)
 ax.set_ylabel(r'$T_0$', fontsize=12)
 fig.savefig('MCchain.pdf', bbox_inches='tight')
+
 # data pic
 fig, ax = plt.subplots(figsize=(8, 8))
-ax.errorbar(n, y, np.transpose(yerr), capsize=3.5, mew=1.5, fmt='.k', alpha=0.5)                 
-x = np.linspace(0, n[-1], 100)
-ax.plot(x, summary[1] + 0 * x)
-ax.fill_between(x, summary[0] + 0 * x, summary[2] + 0 * x,
+ax.errorbar(x, y, np.transpose(yerr), capsize=3.5, mew=1.5, fmt='.k', alpha=0.5)             
+xline = np.linspace(min(x), max(x), 100)
+ax.plot(xline, 0 * xline, 'k')
+ax.plot(xline, model(summary[1], xline))
+ax.fill_between(xline, model(summary[0], xline), model(summary[2], xline),
             facecolor='b', alpha=0.3, color='b', linewidth=2, linestyle='-')
-ax.set_xlabel(r'number of cluster', fontsize=12)
-ax.set_ylabel(r'$T_0$', fontsize=12)
+ax.set_xlabel(r'z', fontsize=12)
+ax.set_ylabel(r'$T_z$', fontsize=12)
 fig.savefig('Data.pdf', bbox_inches='tight')
-# chi2 pic
-fig, ax = plt.subplots(figsize=(8, 8))
-T = np.linspace(2.5, 3., 100)
-ax.plot(T, [-2 * log_prob(t) for t in T])
-ax.set_xlabel(r'$T_0$', fontsize=12)
-ax.set_ylabel(r'$\chi^2$', fontsize=12)
-fig.savefig('Chi2.pdf', bbox_inches='tight')
